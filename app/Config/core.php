22ec95ae7ccac5f27d9223c69c396cabfb1ac98d
fukuda <fukudatatsuya911@gmail.com>
2013-05-28 21:46:17 +0900

    Security.saltとSecurity.cipherSeedの値を更新

--- app/Config/core.php
+++ app/Config/core.php
@@ -194,12 +194,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'PGi04RF44YKs0eBVovFsKzvizEWvlmm8XLTkMeo2');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '28825262948287599979215219546');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
