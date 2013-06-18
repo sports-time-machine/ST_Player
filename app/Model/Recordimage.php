87268db2f848bbe3e938a6b500545a320f8c8d75
mikage <mikage.xp@gmail.com>
2013-06-18 20:04:04 +0900

    ファイル名を修正

--- app/Model/Recordimage.php
+++ app/Model/Recordimage.php
@@ -4,10 +4,10 @@ App::uses('AppModel', 'Model');
 class RecordImage extends AppModel
 {
 	public $name = 'RecordImage';
-	public $useTable = 'recordimages';
+	public $useTable = 'record_images';
 	public $primaryKey = 'id';
 	
-	// app_model.phpでconfig/column_list/Recordimage.php, config/validate/Recordimage.phpを読み込み
+	// app_model.phpでconfig/column_list/RecordImage.php, config/validate/RecordImage.phpを読み込み
 	public $column_list = array();
 	public $validate = array();
 	
