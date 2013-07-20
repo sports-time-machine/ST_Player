a768282569236120cb2b55566574b645446f8d66
mikage <mikage.xp@gmail.com>
2013-07-21 01:14:20 +0900

    ログ取得用のコードを追加

--- app/Model/Behavior/LogBehavior.php
+++ app/Model/Behavior/LogBehavior.php
@@ -9,11 +9,28 @@ class LogBehavior extends ModelBehavior {
 		$this->Log = new Log;
 	}
 	
+	// システムログ
 	public function systemLog(Model $_model, $log, $loglevel = LOG_LEVEL_ERROR, $model = null, $action = null, $affected_id = null) {
 		$this->Log->systemLog($log, $loglevel, $model, $action, $affected_id);
 	}
 	
+	// ユーザー操作のログ
 	public function userLog(Model $_model, $log, $loglevel = LOG_LEVEL_ERROR, $model = null, $action = null, $affected_id = null) {
 		$this->Log->userLog($log, $loglevel, $model, $action, $affected_id);
+	}
+	
+	// saveログ
+	public function afterSave(Model $model, $created) {
+		parent::afterSave($model, $created);
+		//pr($model->data);exit;
+		if ($created) {
+			// INSERTの場合
+			$log = "データを登録しました";
+			$this->userLog($model, $log, LOG_LEVEL_INFO, $model->name, LOG_ACTION_CREATE, $model->data[$model->name]['id']);
+		} else {
+			// UPDATEの場合
+			$log = "データを更新しました";
+			$this->userLog($model, $log, LOG_LEVEL_INFO, $model->name, LOG_ACTION_UPDATE, $model->data[$model->name]['id']);
+		}
 	}
 }
--- app/Model/Log.php
+++ app/Model/Log.php
@@ -4,24 +4,24 @@ class Log extends AppModel {
 	public $useTable = 'logs';
 	public $primaryKey = 'id';
 	// for Search Plugin
-	var $actsAs = array('Search.Searchable');
-	var $filterArgs = array(
+	public $actsAs = array('Search.Searchable');
+	public $filterArgs = array(
 		'log' => array('type' => 'like', 'field' => array('Log.log')),
 	);
 
-	var $log_type_list = array(
+	public $log_type_list = array(
 		LOG_TYPE_SYSTEM => 'SYSTEM',
 		LOG_TYPE_USER => 'USER',
 	);
 	
-	var $log_level_list = array(
+	public $log_level_list = array(
 		LOG_LEVEL_ERROR => 'ERROR',
 		LOG_LEVEL_WARN  => 'WARN',
 		LOG_LEVEL_INFO  => 'INFO',
 		LOG_LEVEL_DEBUG => 'DEBUG',
 	);
 	
-	var $log_action_list = array(
+	public $log_action_list = array(
 		LOG_ACTION_CREATE => '登録',
 		LOG_ACTION_READ   => '表示',
 		LOG_ACTION_UPDATE => '変更',
@@ -31,14 +31,15 @@ class Log extends AppModel {
 		LOG_ACTION_MAIL   => 'メール',
 	);
 	
-	function _log($log, $logtype, $loglevel = LOG_ERROR, $model = null, $action = null, $affected_id = null) {
+	public function _log($log, $logtype, $loglevel = LOG_ERROR, $model = null, $action = null, $affected_id = null) {
 		$logtype = $this->empty2null($logtype);
 		$loglevel = $this->empty2null($loglevel);
 		$affected_id = $this->empty2null($affected_id);
 		$action = $this->empty2null($action);
-		// TODO ログインユーザーを考慮する
-		if (!empty($_SESSION['FW_LOGIN_USER']['User']['username'])) {
-			$username = $_SESSION['FW_LOGIN_USER']['User']['username'];
+		// usernameを保存する
+		$loginUser = $this->getLoginUser();
+		if (!empty($loginUser['User']['username'])) {
+			$username = $loginUser['User']['username'];
 		} else {
 			$username = null;
 		}
@@ -61,7 +62,7 @@ class Log extends AppModel {
 	}
 	
 	// CakePHPのログ出力
-	function _cakeLog($log, $loglevel = LOG_ERROR) {
+	public function _cakeLog($log, $loglevel = LOG_ERROR) {
 		if ($loglevel == LOG_LEVEL_ERROR || $loglevel == LOG_LEVEL_WARN) {
 			$cake_loglevel = 'error';
 		} else {
@@ -71,18 +72,24 @@ class Log extends AppModel {
 	}
 	
 	
-	function systemLog($log, $loglevel = LOG_LEVEL_ERROR, $model = null, $action = null, $affected_id = null) {
+	public function systemLog($log, $loglevel = LOG_LEVEL_ERROR, $model = null, $action = null, $affected_id = null) {
 		// DBに記録
 		$this->_log($log, LOG_TYPE_SYSTEM, $loglevel, $model, $action, $affected_id);
 		// CakePHPのログ出力
 		$this->_cakeLog($log, $loglevel);
 	}
 	
-	function userLog($log, $loglevel = LOG_LEVEL_ERROR, $model = null, $action = null, $affected_id = null) {
+	public function userLog($log, $loglevel = LOG_LEVEL_ERROR, $model = null, $action = null, $affected_id = null) {
 		// DBに記録
 		$this->_log($log, LOG_TYPE_USER, $loglevel, $model, $action, $affected_id);
 		// CakePHPのログ出力
 		$this->_cakeLog($log, $loglevel);
 	}
 	
+	public function empty2null($str) {
+		if (strcmp($str, '') == 0) {
+			$str = null;
+		}
+		return $str;
+	}
 }
