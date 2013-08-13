c9b675ae964d08d894575b014b4421086007f57f
fukuda <fukudatatsuya911@gmail.com>
2013-08-13 11:02:26 +0900

    動物などの記録を追加

--- app/Model/Partner.php
+++ app/Model/Partner.php
@@ -25,12 +25,16 @@ class Partner extends AppModel
             $partner['name'] = $data['Record']['player_id'];
             $partner['is_linked'] = true;
         }else{
+            //動物などの記録の場合
              if (preg_match("/M:/", $partner_id)){
                 $str = substr($partner_id,2);   //2文字以降を取り出す
                 $partner['name']="";
                 if (strcmp($str, "CHEETAH-1") == 0) $partner['name'] = "チーター";
                 if (strcmp($str, "ELEPHANT-3") == 0) $partner['name'] = "アフリカゾウ";
                 if (strcmp($str, "MUSAGI") == 0) $partner['name'] = "ミナコウサギ";
+                if (strcmp($str, "MIZUESAIBOU") == 0) $partner['name'] = "水江未来 細胞";
+                if (strcmp($str, "PETER-1") == 0) $partner['name'] = "Peter Millard 1";
+                if (strcmp($str, "PETER-2") == 0) $partner['name'] = "Peter Millard 2";
                 
                 $partner['record_id'] = $partner_id;
                 $partner['is_linked'] = false;
